88cbaf331883676d36618616f0f9f59ce57ef709
EUNSEO814 <eunseo814@gmail.com>
2024-03-27 22:26:18 +0900

    Day 38: Next JS14 ,loading, error parallel request, suspense ..

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,29 @@
-export default function Page() {
+import Link from "next/link";
+import { resolve } from "path";
+
+export const metadata = {
+  title: "Home",
+};
+
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  // await new Promise((resolve) => setTimeout(resolve, 5000)); //loading 확인용
+  const res = await fetch(API_URL);
+  const json = await res.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+
   return (
     <div>
-      <h1>Hello!</h1>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
     </div>
   );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,23 @@
-export default function MovieDetail({
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie Detail {id}</h1>;
+  return (
+    <>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </>
+  );
 }
+
+// export default const MovieDetail = async() =>{}
